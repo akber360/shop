41ece3740fd6d75ff2a53ca7a3f0580f5055815d
Akber Ali <akber360@yahoo.co.uk>
2023-08-14 11:55:40 +0100

    adding flask excercise 2

---
+++ exercise2/application/routes.py
@@ -0,0 +1,38 @@
+from application import app, db
+from application.models import Games
+from flask import render_template
+
+@app.route('/add')
+def add():
+    new_game = Games(name="New Game")
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to database"
+@app.route('/add1')
+def add1():
+    new_game = Games(name="New Game1231")
+    db.session.add(new_game)
+    db.session.commit()
+    return "Added new game to database"
+
+@app.route('/read')
+def read():
+    all_games = Games.query.all()
+    games_string = ""
+    for game in all_games:
+        games_string += "<br>" + str(game.id) + ": " + game.name
+    return games_string
+
+@app.route('/update/<name>')
+def update(name):
+    first_game = Games.query.first()
+    first_game.name = name
+    db.session.commit()
+    return first_game.name
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    game_to_delete = Games.query.filter_by(id=id).first()
+    db.session.delete(game_to_delete)
+    db.session.commit()
+    return f"The game with ID number; {game_to_delete.id} has been deleted!"
