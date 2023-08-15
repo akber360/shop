372dfaa7c3977c1e649d18290385e7044bf833dd
Akber Ali <akber360@yahoo.co.uk>
2023-08-15 15:10:55 +0100

    added the payment and cart sections branch

--- not-application/application/routes.py
+++ not-application/application/routes.py
@@ -1,15 +1,31 @@
 from application import app, db
 from application.models import Customer,Order,Product,Order_detail
-from flask import render_template, request
-
-@app.route('/')
-def home():
-    return render_template ('index.html')
+from flask import render_template, request, redirect,url_for
 
 @app.route('/cart')
 def cart():
+    all_product = Product.query.all()
     all_orders = Order_detail.query.all()
-    order_string = ""
-    for order in all_orders:
-        order_string += "<br>"+ str(order.order_detail_id) + str(order.order_id) + str(order.quantity)  + str(order.price)
-    return order_string
+    total_amount = sum([order.quantity * order.price for order in all_orders])
+    return render_template('cart.html', all_orders=all_orders, total_amount=total_amount, all_product=all_product) 
+
+@app.route('/update_quantity/<int:order_detail_id>', methods=['POST'])
+def update_quantity(order_detail_id):
+    new_quantity = request.form.get('new_quantity')
+    order_detail = Order_detail.query.get(order_detail_id)    
+    if order_detail:
+        order_detail.quantity = int(new_quantity)
+        db.session.commit()    
+    return redirect(url_for('cart'))
+
+@app.route('/delete_item/<int:order_detail_id>', methods=['POST'])
+def delete_item(order_detail_id):
+    order_detail = Order_detail.query.get(order_detail_id)    
+    if order_detail:
+        db.session.delete(order_detail)
+        db.session.commit()    
+    return redirect(url_for('cart'))  # Removed the form object from the redirect
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
