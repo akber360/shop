5c4135fd934c99881f120358ee2ae1128670138a
Akber Ali <akber360@yahoo.co.uk>
2023-08-15 20:55:02 +0100

    payment page route added

--- not-application/create.py
+++ not-application/create.py
@@ -15,3 +15,15 @@ with app.app_context():
     testuser = Order_detail(order_id=3, product_id=2, quantity=112, price=50.00)
     db.session.add(testuser)
     db.session.commit()
+
+    bike_trail = Product(name="Bike Trail", description="Mountain Bike riding", price=50, stock_level=1)
+    db.session.add(bike_trail)
+    db.session.commit()
+
+    zip_line = Product(name="Zip Line", description="Zip line x 5 goes", price=20, stock_level=1)
+    db.session.add(zip_line)
+    db.session.commit()
+
+    pear = Product(name="pear", description="its a nice pear", price=100, stock_level=100)
+    db.session.add(pear)
+    db.session.commit()
