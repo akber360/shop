91b65cabdb91e97b3532a6933e9362cdc065a855
Akber Ali <akber360@yahoo.co.uk>
2023-08-16 14:50:44 +0100

    last minute html updates

--- not-application/application/routes.py
+++ not-application/application/routes.py
@@ -19,6 +19,10 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/category')
+def category():
+    return render_template('categories.html')
+
 @app.route('/cart')
 def cart():
     all_product = Product.query.all()
@@ -49,15 +53,27 @@ class PayForm(FlaskForm):
         Length(min=16, max=16)])
     cvc_num = StringField('Enter 3 digit CVC Number', validators=[
         DataRequired(),
-        Length(min=3, max=3)])    
+        Length(min=3, max=3)])   
+    first_name = StringField('First Name', validators=[
+        DataRequired(),
+        Length(min=4, max=30)])
+    last_name = StringField('Last Name', validators=[
+        DataRequired(),
+        Length(min=2, max=30)]) 
+    exp_date = StringField('Enter expiery date MM/YY', validators=[
+        DataRequired(),
+        Length(min=5, max=5)]) 
     submit = SubmitField('Pay Now')
+
 
 @app.route('/payment', methods=['GET', 'POST'])
 def payment():
     message = ""
     form = PayForm()
     if request.method == 'POST':
-            message = f'Thank you, Payment has been Accepted' 
+            first_name = form.first_name.data
+            last_name = form.last_name.data
+            message = f'Thank you ,{ first_name} {last_name} Payment has been Accepted' 
     return render_template('payment.html', form=form, message=message)
 
 class ShipForm(FlaskForm):
@@ -85,6 +101,9 @@ class ShipForm(FlaskForm):
 def shipping():
     message = ""
     form = ShipForm()
+    all_product = Product.query.all()
+    all_orders = Order_detail.query.all()
+    total_amount = sum([order.quantity * order.price for order in all_orders])
     if request.method == 'POST':
         if form.validate_on_submit():
             first_name = form.first_name.data
@@ -103,35 +122,39 @@ def shipping():
             # Add and commit the new customer to the database
             db.session.add(customer)
             db.session.commit()
-            message = f'Thank you, {first_name} {last_name}. Payment Accepted' 
-    return render_template('shipping.html', form=form, message=message)
+            message = f'Thank you, {first_name} {last_name}. your shipping details have been saved' 
+    return render_template('shipping.html', form=form, message=message, all_orders=all_orders, total_amount=total_amount, all_product=all_product)
 
 
-@app.route('/product', methods=['GET', 'POST'])
+@app.route('/products', methods=['GET', 'POST'])
 def products():
     products = Product.query.all()
-    return render_template('product.html', products=products)
+    return render_template('products.html', products=products)
+
+@app.route('/product1', methods=['GET', 'POST'])
+def product1():
+    return render_template('product1.html')
+
+@app.route('/product2', methods=['GET', 'POST'])
+def product2():
+    return render_template('product2.html')
 
 @app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
     product_id = request.form.get('product_id')
     product = Product.query.get(product_id)
-    # Check if there's an active cart for the current session
     cart_order = Order.query.filter_by(is_cart=True, status=True).first()  # Find active cart
     if not cart_order:
-        # If no cart, create one
         cart_order = Order(status=True, is_cart=True, order_date=datetime.now())
         db.session.add(cart_order)
         db.session.commit()
     if product:
-        # Create a new Order_detail instance
         order_detail = Order_detail(
             order_id=cart_order.order_id,
             product_id=product.product_id,
-            quantity=1,  # You can modify this to take quantity as an input
+            quantity=1,
             price=product.price
-        )
-        # Add and commit the new order_detail to the database
+        )  
         db.session.add(order_detail)
         db.session.commit()
     return redirect(url_for('products'))
